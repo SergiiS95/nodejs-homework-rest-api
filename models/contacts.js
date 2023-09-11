e5cdb8a86a4175d72f66ff6f741de6bff3ef885c
SergiiS95 <SSirota1995@gmail.com>
2023-09-11 17:03:31 +0300

    update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,24 +1,3 @@
-// // import fs from 'fs/promises'
-
-// const listContacts = async () => {};
-
-// const getContactById = async (contactId) => {};
-
-// const removeContact = async (contactId) => {};
-
-// const addContact = async (body) => {};
-
-// const updateContact = async (contactId, body) => {};
-
-// export default {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-//   updateContact,
-// };
-
-
 import fs from "fs/promises";
 import path from "path";
 import { nanoid } from "nanoid";
